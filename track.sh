28b4d5627dc70b7af618fc061137d5e1525e23fc
chLi <565353780@qq.com>
2023-11-20 16:11:47 +0800

    fix running bug

--- track.sh
+++ track.sh
@@ -2,23 +2,22 @@
 
 PID=$(pgrep ${1})
 
-if [ -z "${PID}" ]; then
-    echo '[ERROR][track.sh]'
-    echo '\t input process not exist!'
-    exit 1
-fi
+echo ${PID}
 
-if [ -z "${2}" ]; then
-    echo '[ERROR][track.sh]'
-    echo '\t need to input tracking time!'
-    exit 1
+if [ -z "${PID}" ]; then
+	echo '[ERROR][track.sh]'
+	echo '\t input process not exist!'
+	exit 1
 fi
 
 mkdir output
 cd output
 
-rm -f perf-${1}*
-perf record -F 99 -p $PID -g -o perf-${1}-in-fb.data -- sleep ${2}
-perf script -i perf-${1}-in-fb.data &> perf-${1}.unfold
-stackcollapse-perf-pl perf-${1}.unfold &> perf-${1}.folded
-flamegraph.pl perf-${1}.folded > perf-${1}.svg
+rm -rf ${1}
+mkdir ${1}
+cd ${1}
+
+perf record -F 99 -p $PID -g -o perf-in-fb.data -- sleep 60
+perf script -i perf-in-fb.data >perf.unfold
+../../../FlameGraph/stackcollapse-perf.pl perf.unfold >perf.folded
+../../../FlameGraph/flamegraph.pl perf.folded >perf.svg
