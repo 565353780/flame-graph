a28d66e7173a61c802923bc945e00f3f1601b50a
chLi <565353780@qq.com>
2023-11-20 16:15:57 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 cd ..
 git clone https://github.com/brendangregg/FlameGraph.git
+
+VERSION=6.5.6-76060506
+
+sudo apt install linux-tools-common
+sudo apt install linux-tools-${VERSION} linux-tools-${VERSION}-generic
